d49849cd22ba5a33194e718b022c1a8207bbc389
Tal Shorer <tal.shorer@gmail.com>
2022-08-25 22:53:55 +0300

    Add simple socket-based cli
    The game engine currently assigns roles (of a limited supported set) and
    gives players the information they're privy to.
    
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

---
+++ avalon.py
@@ -0,0 +1,102 @@
+#! /usr/bin/python3
+
+import random
+import enum
+import collections
+import asyncio
+
+
+class AvalonPlayer:
+    pass
+
+
+class _Side(enum.Enum):
+    GOOD = 1
+    EVIL = 2
+
+
+_Role = collections.namedtuple("_Role", ["key", "name", "side", "know"])
+
+
+class Role(enum.Enum):
+    Minion = _Role(
+        key="Minion",
+        name="Minion of Mordred",
+        side=_Side.EVIL,
+        know=("Minion"),
+    )
+    Servant = _Role(
+        key="Servant",
+        name="Servant of Arthur",
+        side=_Side.GOOD,
+        know=(),
+    )
+    Merlin = _Role(
+        key="Merlin",
+        name="Merlin",
+        side=_Side.GOOD,
+        know=("Minion"),
+    )
+
+
+# TODO make abstract...
+class Player:
+    async def send(self):
+        pass
+
+    async def input(self, kind):
+        pass
+
+
+_Rules = collections.namedtuple("_Rules", ["total_evil"])
+_rules = {
+    5: _Rules(2),
+    6: _Rules(2),
+    7: _Rules(3),
+    8: _Rules(3),
+    9: _Rules(3),
+    10: _Rules(4),
+}
+
+
+async def broadcast(players, msg):
+    await asyncio.gather(*[player.send(msg) for player in players])
+
+
+async def vote(players):
+    results = await asyncio.gather(*[
+        player.input("vote") for player in players])
+    return {player.name: result for player, result in zip(players, results)}
+
+
+async def send_initial_info(player, role, player_map):
+    await player.send(f"Welcome to Avalon, {player.name}!")
+    await player.send(f"Your role is {role.value.name}")
+    know = []
+    for other_player, other_role in player_map:
+        if other_player is player:
+            continue
+        if other_role.value.key in role.value.know:
+            know.append(other_player.name)
+    if know:
+        await player.send("Here are the players you should know about:")
+        await player.send(" ".join(know))
+
+
+async def play(players, roles):
+    active_rules = _rules[len(players)]
+    evils = active_rules.total_evil
+    goods = len(players) - evils
+    evil_roles = [r for r in roles if r.value.side == _Side.EVIL]
+    good_roles = [r for r in roles if r.value.side == _Side.GOOD]
+    if len(evil_roles) > evils:
+        raise ValueError("Too many evil roles")
+    if len(good_roles) > goods:
+        raise ValueError("Too many good roles")
+    evil_roles.extend([Role.Minion] * (evils - len(evil_roles)))
+    good_roles.extend([Role.Servant] * (goods - len(good_roles)))
+    all_roles = evil_roles + good_roles
+    random.shuffle(all_roles)
+    player_map = list(zip(players, all_roles))
+    await asyncio.gather(*[send_initial_info(
+        player, role, player_map) for player, role in player_map])
---
+++ avalon_cli.py
@@ -0,0 +1,86 @@
+#! /usr/bin/python3
+
+import socket
+import asyncio
+
+import avalon
+
+
+class CliPlayer(avalon.Player):
+    def __init__(self, c):
+        self.c = c
+        self.name = asyncio.get_event_loop().run_until_complete(self.read())
+
+    async def read(self):
+        loop = asyncio.get_event_loop()
+        data = b""
+        while not data.endswith(b"\n"):
+            data += await loop.sock_recv(self.c, 0x1000)
+        return data.decode().strip()
+
+    async def input(self, kind=""):
+        loop = asyncio.get_event_loop()
+        await loop.sock_sendall(self.c, b"I\n")
+        data = await self.read()
+        return data
+
+    async def send(self, msg):
+        loop = asyncio.get_event_loop()
+        print(self.name, msg)
+        await loop.sock_sendall(self.c, b"P" + msg.encode() + b"\n")
+
+
+ADDRESS = ("127.0.0.1", 7015)
+
+
+def server():
+    nplayers = 8
+    roles = [avalon.Role.Merlin]
+
+    s = socket.socket()
+    s.bind(ADDRESS)
+    s.listen(nplayers)
+
+    print(f"Waiting for {nplayers} players")
+    players = []
+    while len(players) < nplayers:
+        c, _ = s.accept()
+        c.setblocking(False)
+        players.append(CliPlayer(c))
+    players.sort(key=lambda player: player.name)
+    asyncio.get_event_loop().run_until_complete(avalon.play(players, roles))
+
+
+def client(name):
+    s = socket.socket()
+    s.connect(ADDRESS)
+    s.sendall(name.encode() + b"\n")
+    data = b""
+    while True:
+        s.setblocking(not data)
+        try:
+            data += s.recv(0x100)
+        except BlockingIOError:
+            pass
+        try:
+            idx = data.index(b"\n")
+        except ValueError:
+            continue
+        msg = data[:idx]
+        op = msg[0:1]
+        if op == b"P":
+            print(msg[1:].decode())
+        elif op == b"I":
+            s.sendall(input("> ").encode() + b"\n")
+        else:
+            print(msg.decode())
+
+        data = data[idx+1:]
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        server()
+    else:
+        client(*sys.argv[1:])
